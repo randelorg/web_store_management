46a9186fea52963d218f226587fea0b1bb6d5d61
randel-reyes22 <randel.reyes@unc.edu.ph>
2022-05-30 17:15:49 +0800

    deploy v6

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "dff9d9f1cdde5bdbc15378dca6c52d87",
+  "assets/AssetManifest.json": "c1051d02ebaee8f4cb65896b02633909",
 "assets/assets/fonts/Cairo_Bold.ttf": "80ada5fcee2d9415ee00ef7739eba6df",
 "assets/assets/fonts/Cairo_Light.ttf": "521aad886875e8c23944c69616960523",
 "assets/assets/fonts/Cairo_SemiBold.ttf": "df4b7a4fa0bbf338ddcf7abad1885344",
@@ -29,8 +29,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b1004987446e63f873eabaf329025cef",
-"/": "b1004987446e63f873eabaf329025cef",
+"index.html": "4556a6c5688fb9fb1378a662343f5e04",
+"/": "4556a6c5688fb9fb1378a662343f5e04",
 "index.php": "ad0328aebc71b83400dcdecbfda3371b",
 "main.dart.js": "ec50e347cee35f6d09afff30591fece0",
 "manifest.json": "86f23c25621ff749f73ef1a6a575cc0e",
